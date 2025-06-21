e8ab942094b7c162f9ae0740dbae3acb957fbb61
AbishekSathiyan <abidinesh2213.gmail.com>
2025-06-21 18:16:28 +0530

    All Done Ready to Production

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-light py-8">
-      <div className="container mx-auto px-6 text-center">
-        <p>© {new Date().getFullYear()} Abishek S All rights reserved.</p>
-      </div>
-    </footer>
+   <footer className="bg-gray-900 text-white py-8">
+  <div className="container mx-auto px-6 text-center">
+    <p>© {new Date().getFullYear()} Abishek S. All rights reserved.</p>
+  </div>
+</footer>
+
   );
 }
