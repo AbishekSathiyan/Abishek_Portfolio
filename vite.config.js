b1f43f3a10642328542415f06b673cdac68ab784
AbishekSathiyan <abidinesh2213.gmail.com>
2025-06-21 10:40:05 +0530

    Profile Updated

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  build: {
+    outDir: 'dist'
+  },
   plugins: [react()],
   server: {
-    hmr: { overlay: false } // optional
+    hmr: { overlay: false }
   }
 });
